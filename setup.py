1597fc9298661ec99b475d08c22f56eb36233c86
rightlag <rightlag@gmail.com>
2016-01-27 19:48:32 +0000

    Updated setup.py module to read requirements from requirements.txt file

--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
-import timely
+import os
 
-from setuptools import setup
+from codecs import open
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'requirements.txt'), encoding='utf-8') as fp:
+    requirements = fp.read().splitlines()
 
 setup(
     name='timely',
-    version=timely.__version__,
-    author=timely.__author__,
+    version='0.2.0',
+    author='Jason Walsh',
     author_email='rightlag@gmail.com',
     description='Manage the uptime of Amazon Web Services EC2 containers',
     license='MIT',
     keywords='aws ec2 uptime',
     url='https://github.com/rightlag/timely',
-    packages=['timely', 'tests'],
-    classifiers=['Development Status :: 4 - Beta']
+    packages=find_packages(exclude=['test*']),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+    ],
+    install_requires=requirements
 )
